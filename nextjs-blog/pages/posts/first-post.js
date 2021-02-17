e92fdc9b03a6e39c4e417de608ec869bc733be0d
ken505 <68226398+ken505@users.noreply.github.com>
2021-02-17 16:06:09 +0900

    wip:

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,14 +1,19 @@
-import Link from 'next/link'
+import Link from "next/link";
+import Head from "next/head";
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
   return (
-    <>
-      <h1>First Post</h1>
+    <Layout>
+      <Head>
+        <title>Ken's Room</title>
+      </Head>
+      <h1>Ken's Room</h1>
       <h2>
         <Link href="/">
           <a>Back to home</a>
         </Link>
       </h2>
-    </>
-  )
+    </Layout>
+  );
 }
